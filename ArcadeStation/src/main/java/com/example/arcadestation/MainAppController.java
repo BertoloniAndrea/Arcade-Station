b3cc0dcde211289d40385de2714de7eb976e542b
Berto <105243507+BertoloniAndrea@users.noreply.github.com>
2023-05-04 12:05:40 +0200

    Delete ArcadeStation directory

--- ArcadeStation/src/main/java/com/example/arcadestation/MainAppController.java
+++
@@ -1,4 +0,0 @@
-package com.example.arcadestation;
-
-public class MainAppController {
-}
--- ArcadeStation/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.arcadestation {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.almasb.fxgl.all;
-
-    opens com.example.arcadestation to javafx.fxml;
-    exports com.example.arcadestation;
-}
